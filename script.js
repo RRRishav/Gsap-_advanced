2beac4864964751c3da0cb2a5df3f9b48fb9295c
RRRishav <126879117+RRRishav@users.noreply.github.com>
2024-10-16 04:35:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// gsap.to("#box",{
+//     x:1000,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     duration:2,
+//     delay:1
+// })
+
+//gsap timeline
+var t1 = gsap.timeline()
+
+
+
+t1.to("#box1",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:0.2,
+
+})
+
+t1.to("#box2",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+})
+
+
+t1.to("#box3",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+})
